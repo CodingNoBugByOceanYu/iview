c97158e7783d3cde5fa1654eb9d8106dffb814cf
BigHai <myqq1017482155@gmail.com>
2019-02-20 15:41:52 +0800

    提交填充文件

---
+++ src/application/content.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="content">
+        <Tab></Tab>
+        <Infos></Infos>
+    </div>
+</template>
+
+<script>
+import Tab from './tab.vue'
+import Infos from './infos.vue'
+export default {
+    components: {
+        Tab,
+        Infos
+    }
+}
+</script>
+<style lang="stylus" scoped>
+#content 
+    margin-top 50px
+    margin-left 240px
+</style>
